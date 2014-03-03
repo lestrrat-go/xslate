e9e96997de7b971e2e00fc38a8aa908cfa181ea3
Daisuke Maki <lestrrat+github@gmail.com>
2014-03-04 06:51:11 +0900

    some comments

--- vm/frame.go
+++ vm/frame.go
@@ -10,6 +10,8 @@ type Frame struct {
   retaddr interface {} // TODO: what's this?
 }
 
+// a Frame uses the stack, starting at position `base`. The base is determined
+// by calling st.Pushmark() before calling NewFrame()
 func NewFrame(base int, s *Stack) *Frame {
   return &Frame {
     base: base,
@@ -17,14 +19,17 @@ func NewFrame(base int, s *Stack) *Frame {
   }
 }
 
+// Gets the frame local variable at position i, which is relative from base
 func (f *Frame) GetLvar(i int) interface {} {
   return f.stack.Get(f.base + i)
 }
 
+// Sets the frame local variable at position i, which is relative from base
 func (f *Frame) SetLvar(i int, v interface {}) {
   f.stack.Set(f.base + i, v)
 }
 
+// Returns the index of the last element in our stack, relative from base
 func (f *Frame) LastLvarIndex() int {
   return f.stack.Cur() - f.base
 }
