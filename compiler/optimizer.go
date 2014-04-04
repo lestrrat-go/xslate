f9b0aad5158264c39ed8942df8689a009e40f300
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-04 23:23:49 +0900

    grr, missing file..

---
+++ compiler/optimizer.go
@@ -0,0 +1,30 @@
+package compiler
+
+import (
+  "fmt"
+  "github.com/lestrrat/go-xslate/vm"
+)
+
+type Optimizer interface {
+  Optimize(*vm.ByteCode) error
+}
+
+type NaiveOptimizer struct {}
+
+func (o *NaiveOptimizer) Optimize(bc *vm.ByteCode) error {
+  for i := 0; i < bc.Len(); i++ {
+    op := bc.Get(i)
+    if op == nil {
+      return fmt.Errorf("failed to fetch op %d", i)
+    }
+    switch op.Type() {
+    case vm.TXOPLiteral:
+      if i + 1 < bc.Len() && bc.Get(i + 1).Type() == vm.TXOPPrintRaw {
+        bc.OpList[i] = vm.NewOp(vm.TXOPPrintRawConst, op.ArgString())
+        bc.OpList[i + 1] = vm.NewOp(vm.TXOPNoop)
+        i++
+      }
+    }
+  }
+  return nil
+}
