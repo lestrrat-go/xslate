c2aab6fb9b4c8cd5f735ff73e8b9e869471b3554
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-22 09:00:39 +0900

    add functions

--- functions/array/array.go
+++ functions/array/array.go
@@ -20,6 +20,16 @@ func Size(l []interface {}) int {
   return len(l)
 }
 
+// First returns the first element
+func First(l []interface{}) interface {} {
+  return l[0]
+}
+
+// Last returns the last element
+func Last(l []interface{}) interface {} {
+  return l[len(l) - 1]
+}
+
 // Depot returns the FuncDepot for "array"
 func Depot() *functions.FuncDepot {
   return depot
