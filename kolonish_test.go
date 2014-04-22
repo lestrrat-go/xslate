2790cf3ca1d57749dcf3b189ef471eed3914a8c7
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-22 09:00:44 +0900

    Add tests

--- kolonish_test.go
+++ kolonish_test.go
@@ -21,3 +21,13 @@ func TestKolonish_SimpleString(t *testing.T) {
   c.renderStringAndCompare(`    <:- "Hello, World!" :>`, nil, `Hello, World!`)
   c.renderStringAndCompare(`<: "Hello, World!" -:>    `, nil, `Hello, World!`)
 }
+
+func TestKolonish_Comments(t *testing.T) {
+  c := newKolonCtx(t)
+  defer c.Cleanup()
+
+// XXX TODO
+//  c.renderStringAndCompare(`:# This is a comment`, nil, ``)
+  c.renderStringAndCompare(`    <:- "Hello, World!" :>`, nil, `Hello, World!`)
+  c.renderStringAndCompare(`<: "Hello, World!" -:>    `, nil, `Hello, World!`)
+}
