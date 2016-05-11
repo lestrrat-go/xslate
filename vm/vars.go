462cb96dae9081aede58cb6dc05736f50f279146
Daisuke Maki <lestrrat+github@gmail.com>
2016-05-11 12:07:47 +0900

    only use fmt.Sprintf if given key is not a string

--- vm/vars.go
+++ vm/vars.go
@@ -11,7 +11,10 @@ func (v Vars) Set(k string, x interface{}) {
 
 // Get returns the variable stored in slot `x`
 func (v Vars) Get(k interface{}) (interface{}, bool) {
-	key := fmt.Sprintf("%s", k)
+	key, ok := k.(string)
+	if !ok {
+		key = fmt.Sprintf("%s", k)
+	}
 	x, ok := v[key]
 	return x, ok
 }
